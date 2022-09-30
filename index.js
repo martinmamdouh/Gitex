e7e32e7d68756598912630d3627d9eefc3675d6b
martinmamdouh <martin.mamdouh2014@gmail.com>
2022-09-30 13:36:24 +0400

    pause others when one video start

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function onClickPlay(videoId) {
-  document.getElementById("vid1").webkitRequestFullscreen();
+  document.getElementById(videoId).webkitRequestFullscreen();
+  stopOther(videoId);
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+function stopOther(vidId) {
+  for (var id of ["vid1", "vid2", "vid3", "vid4"]) {
+    if (id == vidId) continue;
+    document.getElementById(id).pause();
+  }
 }
