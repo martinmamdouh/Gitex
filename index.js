63c07a82c7aa6f3274b108d9d99aaaff04e5921e
martinmamdouh <martin.mamdouh2014@gmail.com>
2022-10-08 18:44:14 +0400

    adjus videos IDs

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function onClickPlay(videoId) {
+  console.log(videoId);
   document.getElementById(videoId).webkitRequestFullscreen();
   stopOther(videoId);
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function stopOther(vidId) {
-  for (var id of ["vid1", "vid2", "vid3", "vid4"]) {
+  for (var id of ["vid1", "vid3", "vid4", "vid5", "vid6"]) {
     if (id == vidId) continue;
     document.getElementById(id).pause();
   }
